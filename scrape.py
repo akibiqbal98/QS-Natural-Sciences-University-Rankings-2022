ebbd401f2e47736f9365a06e67fed34c7b304a8a
akibiqbal98 <akibiqbal98@gmail.com>
2022-12-30 20:34:15 +0600

    added location

--- scrape.py
+++ scrape.py
@@ -9,13 +9,14 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 
-columns = ["World Rank" , "University name", "Overall Score", "International Research Network" , "H-Index citations", "Citations per paper" , "Academic reputation", "Employer Reputation"]
+columns = ["World Rank", "University name", "Overall Score", "International Research Network",
+           "H-Index citations", "Citations per paper", "Academic reputation", "Employer Reputation"]
 
 
 # Create an instance of Chrome
 driver = webdriver.Chrome()
 # wait for 10 seconds for the page to load
-driver.implicitly_wait(40)
+driver.implicitly_wait(50)
 
 # Navigate to a website
 driver.get('https://www.topuniversities.com/university-rankings/university-subject-rankings/2022/natural-sciences')
@@ -24,7 +25,7 @@ driver.get('https://www.topuniversities.com/university-rankings/university-subje
 # Find and interact with an element on the page
 XPATH = '//div[@class="ranking_tabs quik-tabs"]//ul//li[2]'
 # Wait for the element to appear
-wait = WebDriverWait(driver, 15)
+wait = WebDriverWait(driver, 50)
 element: WebElement = wait.until(EC.presence_of_element_located((By.XPATH, XPATH)))
 # print(element.get_attribute('outerHTML'))
 # print(element.is_displayed(), element.is_enabled())
@@ -71,25 +72,37 @@ def change_page():
 def get_data():
     # data
     CSS_SELECTOR_RANK = 'div#ranking-data-load_ind div._univ-rank'
-    ranks: list[WebElement] = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_RANK)))
+    ranks: list[WebElement] = wait.until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_RANK)))
+
     CSS_SELECTOR_UNI = 'div#ranking-data-load_ind a.uni-link'
     universities: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_UNI)))
+
+    CSS_SELECTOR_COUNTRY = 'div#ranking-data-load_ind div.location'
+    location: list[WebElement] = wait.until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_COUNTRY)))
+
     CSS_SELECTOR_OS = 'div#ranking-data-load_ind div.overall-score-span-ind.overall div.td-wrap-in'
     os: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_OS)))
+
     CSS_SELECTOR_IRN = 'div#ranking-data-load_ind div.overall-score-span-ind.ind_15 div.td-wrap-in'
     irn: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_IRN)))
+
     CSS_SELECTOR_HC = 'div#ranking-data-load_ind div.overall-score-span-ind.ind_69 div.td-wrap-in'
     hc: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_HC)))
+
     CSS_SELECTOR_CPP = 'div#ranking-data-load_ind div.overall-score-span-ind.ind_70 div.td-wrap-in'
     cpp: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_CPP)))
+
     CSS_SELECTOR_AR = 'div#ranking-data-load_ind div.overall-score-span-ind.ind_76 div.td-wrap-in'
     ar: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_AR)))
+
     CSS_SELECTOR_ER = 'div#ranking-data-load_ind div.overall-score-span-ind.ind_77 div.td-wrap-in'
     er: list[WebElement] = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, CSS_SELECTOR_ER)))
@@ -97,6 +110,7 @@ def get_data():
     return zip(
         map(lambda x: x.text, ranks),
         map(lambda x: x.text, universities),
+        map(lambda x: x.text, location),
         map(lambda x: x.text, os),
         map(lambda x: x.text, irn),
         map(lambda x: x.text, hc),
@@ -110,37 +124,41 @@ all_elements = []
 page_no = 51
 
 for indx in range(page_no):
-    sleep(0.5)
-    all_elements.extend(get_data())
-    print(len(all_elements))
-    if indx != page_no - 1:
-        change_page()
-
+    while(True):
+        try:
+            all_elements.extend(get_data())
+            
+        except Exception as e:
+            print(e)
+            continue
+        
+        else:
+            print(len(all_elements))
+            if indx != page_no - 1:
+                change_page()
+                sleep(1) 
+            break
 
 def format_scrappy_data(data: list[tuple]):
     return [
         {
             "World Rank": a,
             "University name": b,
-            "Overall Score": c,
-            "International Research Network": d,
-            "H-Index citations": e,
-            "Citations per paper": f,
-            "Academic reputation": g,
-            "Employer Reputation": h,
-        } for a, b, c, d, e, f, g, h in data
+            "Location": c,
+            "Overall Score": d,
+            "International Research Network": e,
+            "H-Index citations": f,
+            "Citations per paper": g,
+            "Academic reputation": h,
+            "Employer Reputation": i,
+        } for a, b, c, d, e, f, g, h, i in data
     ]
 
 
 # print formatted data using json to a file
-# with open('data.json', 'w') as f:
-#     json.dump(format_scrappy_data(all_elements), f, indent=2)
 
-# df = pd.read_json('data.json')
-# df = pd.DataFrame(df, columns=columns)
-# df.to_csv("QS_World_University_Rankings_by_Subject_2022_Natural_Sciences")
-
-
+with open('data.json', 'w') as f:
+    json.dump(format_scrappy_data(all_elements), f, indent=2)
 
 
 # pause for 10 seconds
